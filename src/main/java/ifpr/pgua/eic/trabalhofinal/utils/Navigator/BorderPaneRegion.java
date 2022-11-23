870911ed23065bc75451194f95dd0efd77c48610
André Ricardo Morato Rosa <andrericardo_182_@hotmail.com>
2022-11-23 11:29:36 -0300

    configurado o ambiente
    criado o projeto, configurados os arquivos e copiados arquivos necessarios do professor hugo

---
+++ src/main/java/ifpr/pgua/eic/trabalhofinal/utils/Navigator/BorderPaneRegion.java
@@ -0,0 +1,5 @@
+package ifpr.pgua.eic.trabalhofinal.utils.Navigator;
+
+public enum BorderPaneRegion {
+    CENTER,LEFT,RIGHT,BOTTOM,TOP;
+}
---
+++ src/main/java/ifpr/pgua/eic/trabalhofinal/utils/Navigator/ScreenRegistry.java
@@ -0,0 +1,7 @@
+package ifpr.pgua.eic.trabalhofinal.utils.Navigator;
+
+import javafx.scene.Parent;
+
+public interface ScreenRegistry {
+    Parent getRoot();
+}
